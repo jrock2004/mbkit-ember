d1edec631195cd212781b696387b75a9bc4a2f04
John Costanzo <jrock2004@gmail.com>
2020-09-05 12:46:10 -0400

    Implementing radio, textarea, and starting select

---
+++ tests/integration/components/radio-test.js
@@ -0,0 +1,27 @@
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+
+module("Integration | Component | radio", function (hooks) {
+  setupRenderingTest(hooks);
+
+  test("Radio input is render", async function (assert) {
+    await render(hbs`<Radio /> Item`);
+
+    assert.dom(".radio").exists();
+    assert.equal(this.element.textContent.trim(), "Item");
+  });
+
+  test("Radio input is selected", async function (assert) {
+    await render(hbs`<Radio @isChecked={{true}} /> Item`);
+
+    assert.dom(".radio").isChecked();
+  });
+
+  test("Radio input is disabled", async function (assert) {
+    await render(hbs`<Radio disabled /> Item`);
+
+    assert.dom(".radio").isDisabled();
+  });
+});
---
+++ tests/integration/components/textarea-test.js
@@ -0,0 +1,32 @@
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+
+module("Integration | Component | textarea", function (hooks) {
+  setupRenderingTest(hooks);
+
+  test("Text area is rendered", async function (assert) {
+    await render(hbs`<Textarea />`);
+
+    assert.dom(".textarea").exists();
+  });
+
+  test("Text area has a value", async function (assert) {
+    await render(hbs`<Textarea @value="Some content" />`);
+
+    assert.dom(".textarea").hasText("Some content");
+  });
+
+  test("Text area is invalid", async function (assert) {
+    await render(hbs`<Textarea @value="Some content" @isInvalid={{true}} />`);
+
+    assert.dom(".textarea").hasClass("invalid").hasAttribute("aria-invalid");
+  });
+
+  test("Text area is disabled", async function (assert) {
+    await render(hbs`<Textarea @value="Some content" disabled />`);
+
+    assert.dom(".textarea").isDisabled();
+  });
+});
