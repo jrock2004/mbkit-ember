30f524e8ba59355fbf516133566f6f21ea96a679
John Costanzo <jrock2004@gmail.com>
2020-09-08 08:01:50 -0400

    Adding some things per docs

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  name: require('./package').name
+  name: require("./package").name,
+  include: function () {
+    this._super.included.apply(this, arguments);
+  },
 };
